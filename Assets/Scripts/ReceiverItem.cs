4accbd87dc329ab9f6db55baeeb88620f78a7f69
taesiri <taesiri@live.com>
2014-03-05 18:18:52 +0330

    Signal Sender

---
+++ Assets/Scripts/ReceiverItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ReceiverItem : MonoBehaviour
+    {
+        public GameObject Receiver;
+        public string Action = "OnSignal";
+        public float Delay;
+
+        public IEnumerator SendWithDelay(MonoBehaviour sender)
+        {
+            yield return new WaitForSeconds(Delay);
+
+            if (Receiver)
+                Receiver.SendMessage(Action);
+            else
+            {
+                Debug.LogWarning("There is no receiver");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SignalSender.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SignalSender : MonoBehaviour
+    {
+        public bool OnlyOnce;
+        public ReceiverItem[] Receivers;
+
+        private bool _hasFired;
+
+        public void SendSignals(MonoBehaviour sender)
+        {
+            if (!_hasFired || !OnlyOnce)
+            {
+                for (int i = 0; i < Receivers.Length; i++)
+                {
+                    sender.StartCoroutine(Receivers[i].SendWithDelay(sender));
+                }
+                _hasFired = true;
+            }
+        }
+    }
+}
