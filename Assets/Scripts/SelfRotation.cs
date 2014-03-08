2968e0980f9e4af6ce89905305656eba0adbc7c2
taesiri <taesiri@live.com>
2014-03-08 14:12:29 +0330

    Simple Rotation

---
+++ Assets/Scripts/SelfRotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SelfRotation : MonoBehaviour
+    {
+        public float RotationSpeed = 10.0f;
+        private Transform _transform;
+
+        public void Start()
+        {
+            _transform = transform;
+        }
+
+        private void Update()
+        {
+            _transform.Rotate(Vector3.forward, Time.deltaTime*RotationSpeed);
+        }
+    }
+}
